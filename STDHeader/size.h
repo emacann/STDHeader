dd971ea63556ad1e3dd340fc5ac68d160582ad6c
Emanuele Canessa <ema0185@gmail.com>
2015-06-12 14:53:17 +0200

    Aggiunto supporto __INTxx_TYPE__

--- STDHeader/size.h
+++ STDHeader/size.h
@@ -37,14 +37,53 @@
 ////////////////////////////////////////////////////////////////////////////////
 /// Integer types //////////////////////////////////////////////////////////////
 
+#if defined(__INT8_TYPE__)
+typedef __INT8_TYPE__       int8_t;
+#else
 typedef signed char         int8_t;
+#endif
+
+#if defined(__UINT8_TYPE__)
+typedef __UINT8_TYPE__      uint8_t;
+#else
 typedef unsigned char       uint8_t;
+#endif
+
+#if defined(__INT16_TYPE__)
+typedef __INT16_TYPE__      int16_t;
+#else
 typedef signed short        int16_t;
+#endif
+
+#if defined(__UINT16_TYPE__)
+typedef __UINT16_TYPE__     uint16_t;
+#else
 typedef unsigned short      uint16_t;
+#endif
+
+#if defined(__INT32_TYPE__)
+typedef __INT32_TYPE__      int32_t;
+#else
 typedef signed int          int32_t;
+#endif
+
+#if defined(__UINT32_TYPE__)
+typedef __UINT32_TYPE__     uint32_t;
+#else
 typedef unsigned int        uint32_t;
+#endif
+
+#if defined(__INT64_TYPE__)
+typedef __INT64_TYPE__      int64_t;
+#else
 typedef signed long long    int64_t;
+#endif
+
+#if defined(__UINT64_TYPE__)
+typedef __UINT64_TYPE__     uint64_t;
+#else
 typedef unsigned long long  uint64_t;
+#endif
 
 typedef int64_t             intmax_t;
 typedef uint64_t            uintmax_t;
