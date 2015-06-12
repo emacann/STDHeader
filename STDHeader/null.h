b5c6c28382f04487b0ea82270392ea6437d9abfe
Emanuele Canessa <ema0185@gmail.com>
2015-06-12 15:24:36 +0200

    Aggiunto supporto __INTxx_MAX/MIN__ + Intentazione

--- STDHeader/null.h
+++ STDHeader/null.h
@@ -5,13 +5,13 @@
 /// NULL Value /////////////////////////////////////////////////////////////////
 
 #ifdef NULL
-#undef NULL
+ #undef NULL
 #endif // !NULL
 
 #if defined(__cplusplus) && __cplusplus > 199711L
-#define NULL    nullptr
+ #define NULL   nullptr
 #else // C or older versions of C++
-#define NULL    0
+ #define NULL   0
 #endif // !__cplusplus
 
 #endif // !_NULL_H
--- STDHeader/size.h
+++ STDHeader/size.h
@@ -14,76 +14,76 @@
 
 /// Windows
 #if (_WIN32 || _WIN64)
-#if _WIN64
-#define ENV_64
-#else
-#define ENV_32
-#endif
+ #if _WIN64
+  #define ENV_64
+ #else
+  #define ENV_32
+ #endif
 #endif
 
 /// GCC
 #if __GNUC__
-#if __x86_64__ || __ppc64__
-#define ENV_64
-#else
-#define ENV_32
-#endif
+ #if __x86_64__ || __ppc64__
+  #define ENV_64
+ #else
+  #define ENV_32
+ #endif
 #endif
 
 #if !defined(ENV_32) && !defined(ENV_64)
-#error Unrecognized environment.
+ #error Unrecognized environment.
 #endif // (ENV_32 || ENV_64)
 
 ////////////////////////////////////////////////////////////////////////////////
 /// Integer types //////////////////////////////////////////////////////////////
 
 #if defined(__INT8_TYPE__)
-typedef __INT8_TYPE__       int8_t;
+ typedef __INT8_TYPE__      int8_t;
 #else
-typedef signed char         int8_t;
-#endif
+ typedef signed char        int8_t;
+#endif // !__INT8_TYPE__
 
 #if defined(__UINT8_TYPE__)
-typedef __UINT8_TYPE__      uint8_t;
+ typedef __UINT8_TYPE__     uint8_t;
 #else
-typedef unsigned char       uint8_t;
-#endif
+ typedef unsigned char      uint8_t;
+#endif // !__UINT8_TYPE__
 
 #if defined(__INT16_TYPE__)
-typedef __INT16_TYPE__      int16_t;
+ typedef __INT16_TYPE__     int16_t;
 #else
-typedef signed short        int16_t;
-#endif
+ typedef signed short       int16_t;
+#endif // !__INT16_TYPE__
 
 #if defined(__UINT16_TYPE__)
-typedef __UINT16_TYPE__     uint16_t;
+ typedef __UINT16_TYPE__    uint16_t;
 #else
-typedef unsigned short      uint16_t;
-#endif
+ typedef unsigned short     uint16_t;
+#endif // !__UINT16_TYPE__
 
 #if defined(__INT32_TYPE__)
-typedef __INT32_TYPE__      int32_t;
+ typedef __INT32_TYPE__     int32_t;
 #else
-typedef signed int          int32_t;
-#endif
+ typedef signed int         int32_t;
+#endif // !__INT32_TYPE__
 
 #if defined(__UINT32_TYPE__)
-typedef __UINT32_TYPE__     uint32_t;
+ typedef __UINT32_TYPE__    uint32_t;
 #else
-typedef unsigned int        uint32_t;
-#endif
+ typedef unsigned int       uint32_t;
+#endif // !__UINT32_TYPE__
 
 #if defined(__INT64_TYPE__)
-typedef __INT64_TYPE__      int64_t;
+ typedef __INT64_TYPE__     int64_t;
 #else
-typedef signed long long    int64_t;
-#endif
+ typedef signed long long   int64_t;
+#endif // !__INT64_TYPE__
 
 #if defined(__UINT64_TYPE__)
-typedef __UINT64_TYPE__     uint64_t;
+ typedef __UINT64_TYPE__    uint64_t;
 #else
-typedef unsigned long long  uint64_t;
-#endif
+ typedef unsigned long long uint64_t;
+#endif // !__UINT64_TYPE__
 
 typedef int64_t             intmax_t;
 typedef uint64_t            uintmax_t;
@@ -91,21 +91,69 @@ typedef uint64_t            uintmax_t;
 ////////////////////////////////////////////////////////////////////////////////
 /// Defines ////////////////////////////////////////////////////////////////////
 
-#define	INT8_MAX            (127i8)
-#define	INT8_MIN            (-INT8_MAX - 1)
-#define	UINT8_MAX           (0xFFui8)
+#if defined(__INT8_MAX__)
+ #undef INT8_MAX
+ #define INT8_MAX           __INT8_MAX__
+#else
+ #define INT8_MAX           (127i8)
+#endif // !__INT8_MAX__
 
-#define	INT16_MAX           (32767i16)
-#define	INT16_MIN           (-INT16_MAX - 1)
-#define	UINT16_MAX          (0xFFFFui16)
+#define INT8_MIN            (-INT8_MAX - 1)
 
-#define	INT32_MAX           (2147483647i32)
-#define	INT32_MIN           (-INT32_MAX - 1)
-#define	UINT32_MAX          (0xFFFFFFFFui32)
+#if defined(__UINT8_MAX__)
+ #undef UINT8_MAX
+ #define UINT8_MAX          __UINT8_MAX__
+#else
+ #define UINT8_MAX          (0xFFui8)
+#endif // !__UINT8_MAX__
 
-#define	INT64_MAX           (9223372036854775807i64)
-#define	INT64_MIN           (-INT64_MAX - 1)
-#define	UINT64_MAX          (0xFFFFFFFFFFFFFFFFui64)
+#if defined(__INT16_MAX__)
+ #undef INT16_MAX
+ #define INT16_MAX          __INT16_MAX__
+#else
+ #define INT16_MAX          (32767i16)
+#endif // !__INT16_MAX__
+
+#define INT16_MIN           (-INT16_MAX - 1)
+
+#if defined(__UINT16_MAX__)
+ #undef UINT16_MAX
+ #define UINT16_MAX         __UINT16_MAX__
+#else
+ #define UINT16_MAX         (0xFFFFui16)
+#endif // !__UINT16_MAX__
+
+#if defined(__INT32_MAX__)
+ #undef INT32_MAX
+ #define INT32_MAX          __INT32_MAX__
+#else
+ #define INT32_MAX          (2147483647i32)
+#endif // !__INT32_MAX__
+
+#define INT32_MIN           (-INT32_MAX - 1)
+
+#if defined(__UINT32_MAX__)
+ #undef UINT32_MAX
+ #define UINT32_MAX         __UINT32_MAX__
+#else
+ #define UINT32_MAX         (0xFFFFFFFFui32)
+#endif // !__UINT32_MAX__
+
+#if defined(__INT64_MAX__)
+ #undef INT64_MAX
+ #define INT64_MAX          __INT64_MAX__
+#else
+ #define INT64_MAX          (9223372036854775807i64)
+#endif // !__INT64_MAX__
+
+#define INT64_MIN           (-INT64_MAX - 1)
+
+#if defined(__UINT64_MAX__)
+ #undef UINT64_MAX
+ #define UINT64_MAX         __UINT64_MAX__
+#else
+ #define UINT64_MAX         (0xFFFFFFFFFFFFFFFFui64)
+#endif // !__UINT64_MAX__
 
 ////////////////////////////////////////////////////////////////////////////////
 /// Size type //////////////////////////////////////////////////////////////////
@@ -113,11 +161,11 @@ typedef uint64_t            uintmax_t;
 #ifndef _SIZE_T_DEFINED
 #define _SIZE_T_DEFINED
 #if defined(ENV_64)
-typedef uint64_t            size_t;
-#define SIZE_T_MAX          UINT64_MAX
+ typedef uint64_t           size_t;
+ #define SIZE_T_MAX         UINT64_MAX
 #elif defined(ENV_32)
-typedef uint32_t            size_t;
-#define SIZE_T_MAX          UINT32_MAX
+ typedef uint32_t           size_t;
+ #define SIZE_T_MAX         UINT32_MAX
 #endif // !ENV_64
 #endif // !_SIZE_T_DEFINED
 
@@ -129,19 +177,19 @@ typedef void *              ptr_t;
 #ifndef _INTPTR_T_DEFINED
 #define _INTPTR_T_DEFINED
 #if defined(ENV_64)
-typedef int64_t             intptr_t;
+ typedef int64_t            intptr_t;
 #elif defined(ENV_32)
-typedef int32_t             intptr_t;
+ typedef int32_t            intptr_t;
 #endif // !ENV_64
 #endif // !_INTPTR_T_DEFINED
 
 #ifndef _UINTPTR_T_DEFINED
 #define _UINTPTR_T_DEFINED
-#if defined(ENV_64)
-typedef uint64_t            uintptr_t;
-#elif defined(ENV_32)
-typedef uint32_t            uintptr_t;
-#endif // !ENV_64
+ #if defined(ENV_64)
+  typedef uint64_t           uintptr_t;
+ #elif defined(ENV_32)
+  typedef uint32_t           uintptr_t;
+ #endif // !ENV_64
 #endif // !_UINTPTR_T_DEFINED
 
 ////////////////////////////////////////////////////////////////////////////////
